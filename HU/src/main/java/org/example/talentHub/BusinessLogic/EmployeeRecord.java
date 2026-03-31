38579c606ea244e6a133c2bbaab9af33b0c5f948
lmejiacoronado-arch <lmejiacoronado@gmail.com>
2026-03-31 08:36:27 -0500

    feat: implement employee business logic

---
+++ HU/src/main/java/org/example/talentHub/BusinessLogic/EmployeeRecord.java
@@ -0,0 +1,48 @@
+package org.example.talentHub.BusinessLogic;
+
+public record EmployeeRecord(String name, int employeeId, double baseSalary, double monthlyBonus, int testScore, int age, int headquarterId, boolean isActive) {
+
+    public double calculateFinalSalary(){
+        return (baseSalary + (monthlyBonus * 1.10)) - (baseSalary * 0.05);
+        /*Execution order:
+        1. (monthlyBonus * 1.10) -> Multiplication inside parentheses.
+        2. (baseSalary * 0.05)  -> Multiplication inside parentheses (Discount).
+        3. baseSalary + Result1 -> Addition.
+        4. Result3 - Result2 -> Final subtraction.*/
+    }
+
+    public double calculateExtraBonus(){
+        int extraBonus = 0; /*Variable initialized in case the condition is met*/
+        if ((employeeId % 2 == 0)) {
+            extraBonus += 2_000_000;
+            /*// The % operator returns the remainder. If it is 0, the number is even.*/
+        }
+        return extraBonus;
+        /*Since the condition was applied, it adds to the initialized variable*/
+    }
+
+    public boolean validateEligibility(){
+        return (testScore > 85 && age < 30) || (headquarterId == 1 && !isActive);
+        /*Operator precedence:
+        1. ! (NOT) -> Negates 'isActive' first.
+        2. && (AND) -> Evaluates the comparison groups.
+        3. || (OR) -> Evaluates the final result between the two blocks.*/
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        EmployeeRecord emp1 = new EmployeeRecord("Luis", 1, 8_000_000, 3_000_000, 82, 22, 3, false);
+
+        System.out.println("--- Employee Report ---");
+        System.out.println("Name: " + emp1.name());
+        System.out.println("Final Salary: $" + emp1.calculateFinalSalary());
+        System.out.println("Extra Bonus (by even ID): $" + emp1.calculateExtraBonus());
+        System.out.println("Is eligible?: " + (emp1.validateEligibility() ? "Yes" : "No"));
+
+        System.out.println("Name length: " + emp1.name().length());
+        /*This print is to visualize the error; to do so, change 'Luis' in the constructor to null.
+          I added .length to operate with that null, because if I don't operate on it,
+          the error won't trigger even if the attribute is null.*/
+    }
+}
